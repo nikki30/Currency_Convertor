2f02326c04827494541718b5fb8f27dd609a907a
nikki30 <nikita.siva@gmail.com>
2015-06-16 18:56:13 +0530

    1 Change
    My first query

--- queryfinal.php
+++ queryfinal.php
@@ -7,6 +7,30 @@
 		if (mysqli_connect_errno())
 			{die ("Connection Failed!". mysqli_connect_errno());
 		    }
-		    else {echo "Connected!";}
 		    ?>
 
+<html>
+<head>
+	<title>
+		Query php
+	</title>
+</head>
+	<body>
+		<?php 
+		$sql="SELECT * FROM members_table";
+		$res=mysqli_query($conn,$sql);
+		if(!$res)
+			{ die ("Query failed!");
+	        }
+	    while($row=mysqli_fetch_row($res))
+	    	{ var_dump($row);
+	    	  echo "<br /><hr /><br />";
+	    	}
+	    	mysqli_free_result($res);
+	    ?>
+	</body>
+</html>
+
+<?php
+mysqli_close($conn);
+?>
